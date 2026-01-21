f91b95eaa2005494c4a139c8195794b876992215
Erwin Mora <erwinmm17@outlook.com>
2026-01-21 11:51:54 -0600

    Middleware para limitar las peticiones al servidor

--- ms-auth/index.js
+++ ms-auth/index.js
@@ -4,6 +4,7 @@ import CORS from "cors";
 
 import conexion from "./src/config/conexion.js";
 import authroute from "./src/routes/auth.routes.js";
+import limiter from "./src/middleware/rate.limit.js";
 
 dotenv.config(); // Cargar variables de entorno
 conexion(); // Conexión a la base de datos
@@ -12,7 +13,7 @@ const uri = process.env.URI_AUTH;
 const app = express(); // Crear la instancia de la apliación express
 app.use(CORS()); // Habilitar los CORS
 app.use(express.json()); // Habilitar JSON para las peticiones
-
+app.use(limiter); // Limitar las peticiones al servidor
 app.use(uri, authroute); // Rutas de la autntificación
 
 
