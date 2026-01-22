db0190ff1f9281b3a150d3f5a2a5103d3692fe78
Erwin Mora <erwinmm17@outlook.com>
2026-01-21 23:22:45 -0600

    Conexión a base de datos e importación de la conexión

--- ms-users/index.js
+++ ms-users/index.js
@@ -2,7 +2,10 @@ import express from "express"; // Libreria para crear el servidor
 import dotenv from "dotenv"; // Libreria para manejar las variables de entorno
 import CORS from "cors"; // Libreria para manejar CORS
 
+import conexion from "./src/config/conexion.js"; // Importar archivo de configuración a la base de datos
+
 dotenv.config(); // Cargar variables de entorno
+conexion(); // Conexión a la base de datos
 const app = express(); // Crear la instancia de la apliación express
 app.use(express.json()); // Habilitar Json para las peticiones
 app.use(CORS()); // Habilitamos CORS para las peticiones
---
+++ ms-users/src/config/conexion.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv"; // Libreria para manejar las variables de entorno
+import mongoose from "mongoose"; // Libreria para implementar MongoDB
+
+dotenv.config(); // Cargar variables de entorno
+const conn = process.env.MONGO_URI; // Cadane de conexión a la base de datos
+
+const conexion = async () => {
+    if (!conn) {
+        console.log("No se encuentra conexión.");
+        return;
+    }
+
+    try {
+        await mongoose.connect(conn)
+        console.log("Conexión con la base de datos establecida.");
+    } catch (error) {
+        console.log("No se estableció conexión.");
+        console.error(error);
+    }
+
+    process.on("SIGINT", async () => {
+        await mongoose.connection.close();
+        console.log("Conexión Mongo cerrada.");
+        process.exit(0);
+    });
+}
+
+export default conexion;
