7d460fc5aeeb65b518a7f566677c2587928eaece
Erwin Mora <erwinmm17@outlook.com>
2026-01-21 23:19:22 -0600

    Archivo de entrada a la aplicación y cambio en package.json

--- ms-users/index.js
+++ ms-users/index.js
@@ -1,5 +1,14 @@
-import express from "express";
-import dotenv from "dotenv";
-import CORS from "cors";
+import express from "express"; // Libreria para crear el servidor
+import dotenv from "dotenv"; // Libreria para manejar las variables de entorno
+import CORS from "cors"; // Libreria para manejar CORS
 
+dotenv.config(); // Cargar variables de entorno
+const app = express(); // Crear la instancia de la apliación express
 app.use(express.json()); // Habilitar Json para las peticiones
+app.use(CORS()); // Habilitamos CORS para las peticiones
+
+const port = process.env.PORT || 3000; // Puerto del servidor
+
+app.listen(port, () => {
+    console.log(`Microservicio de usuarios corriendo en el puerto ${port}`);
+});
