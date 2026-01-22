80b97f8e2f1d6607656f57d44f920bfbc1875516
Erwin Mora <erwinmm17@outlook.com>
2026-01-21 23:36:42 -0600

    Creación del userModel

---
+++ ms-users/src/models/userModel.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    nombre: { type: String, required: true },
+    apellidos: { type: String, required: true },
+    fechanacimiento: { type: Date, required: true },
+    telefono: { type: String, required: true, unique: true },
+    placas: { type: String, required: true, unique: true },
+    ine: { type: String, required: true, unique: true }
+});
+
+export default mongoose.model("User", userSchema);
