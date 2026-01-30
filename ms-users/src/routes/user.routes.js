49075537848462d06f4a2fa68c5258a69f2f4abb
Erwin Mora <erwinmm17@outlook.com>
2026-01-29 18:27:41 -0600

    Archivos de cero ms-users

--- ms-users/src/routes/user.routes.js
+++ ms-users/src/routes/user.routes.js
@@ -1,14 +1,1 @@
-import express from "express";
-import dotenv from "dotenv";
 
-import { createUserController } from "../controller/user.controller.js"
-
-dotenv.config(); // Cargar variables de entorno
-
-const createuser = process.env.URI_CREATE;
-
-const router = express.Router();
-
-router.post(createuser, createUserController);
-
-export default router;
